e784e2e520cd7496539ed64c2d44a5cdc4bbcead
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-04-27 19:45:28 +0200

    add function that clears islandToCompare after the close button

--- src/components/ComparativeModal.jsx
+++ src/components/ComparativeModal.jsx
@@ -1,19 +1,26 @@
 import ReactDOM from 'react-dom'
 import { useParams } from 'react-router-dom'
 import { useGlobalContext } from '../context/GlobalContext'
+import { useComparativeContext } from '../context/ComparativeContext'
 
 
 export default function ComparativeModal({ onClose = () => { }, show }) {
 
     const { id } = useParams()
     const { getIsolaById } = useGlobalContext()
+    const { setIslandToCompare } = useComparativeContext()
+
+    function handleClose() {
+        setIslandToCompare([])
+        onClose()
+    }
 
     return (
         show && ReactDOM.createPortal(
             <div className='modal-container'>
                 <div className='custom-modal'>
 
-                    <button className='btn' onClick={onClose}>Chiudi</button>
+                    <button className='btn' onClick={handleClose} >Chiudi</button>
                 </div>
 
 
