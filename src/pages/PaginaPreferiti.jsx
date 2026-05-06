ff5f4de449c7b25155c442c93693bf313365cc48
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-05-06 12:32:06 +0200

    add case if favourites is empty in the favourite page

--- src/pages/PaginaPreferiti.jsx
+++ src/pages/PaginaPreferiti.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header"
 import { useNavigate } from "react-router-dom"
 import IslandCard from "../components/IslandCard"
 import Footer from "../components/Footer"
+import { NavLink } from "react-router-dom"
 
 export default function PaginaPreferiti() {
 
@@ -21,12 +22,24 @@ export default function PaginaPreferiti() {
                         <i className="bi bi-arrow-left back-arrow arrow-back"></i>Torna indietro
                     </button>
                     <div className="row">
-                        {
-                            favourites.map(favourite => (
-                                <IslandCard isola={favourite} />
-                            ))
-                        }
+                        {favourites.length === 0 ? (
+                            <div className="text-center w-100 pt-5">
+                                <h2 className="mt-4">
+                                    Nessun'isola aggiunta ai preferiti
+                                </h2>
 
+                                <NavLink to="/isole">
+                                    <button className="btn-full-island mt-3">
+                                        Torna alle isole e aggiungine qualcuna!
+                                    </button>
+                                </NavLink>
+                            </div>
+
+                        ) : (
+                            favourites.map(favourite => (
+                                <IslandCard key={favourite.id} isola={favourite} />
+                            ))
+                        )}
                     </div>
 
                 </div>
